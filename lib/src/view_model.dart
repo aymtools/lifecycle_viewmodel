71730d34878f14ed0ac526fd5169892e478a2485
aym <anyaming0@gmail.com>
2024-07-06 14:16:56 +0800

    由于lifecycleExtData的存在使当前package更加简易

--- lib/src/view_model.dart
+++ lib/src/view_model.dart
@@ -1,10 +1,6 @@
-import 'dart:async';
-import 'dart:collection';
-
 import 'package:an_lifecycle_cancellable/an_lifecycle_cancellable.dart';
 import 'package:anlifecycle/anlifecycle.dart';
 import 'package:cancellable/cancellable.dart';
-import 'package:weak_collections/weak_collections.dart' as weak;
 
 abstract class ViewModel {
   bool _mCleared = false;
@@ -12,9 +8,17 @@ abstract class ViewModel {
 
   void onCleared() {}
 
-  addCloseable(Cancellable closeable) {
+  void addCloseable(Cancellable closeable) {
     if (_mCleared) return;
     _closeables.add(closeable);
+  }
+}
+
+extension ViewModelExt on ViewModel {
+  void onDispose(void Function() onDispose) {
+    Cancellable cancellable = Cancellable();
+    cancellable.onCancel.then((_) => onDispose());
+    addCloseable(cancellable);
   }
 }
 
@@ -29,6 +33,12 @@ extension _ViewModelClean on ViewModel {
     onCleared();
   }
 }
+
+// class _ViewModels<T>{
+//   final T instanc;
+//   final Set<Cancellable> _closeables = {};
+//   bool _mCleared = false;
+// }
 
 class ViewModelStore {
   final Map<String, ViewModel> mMap = {};
@@ -103,27 +113,19 @@ class ViewModelProvider {
       _ViewModelDefFactories._instance.addFactory(factory);
 }
 
-Map<Lifecycle, ViewModelStore> _viewModelStore = HashMap();
-Map<Lifecycle, ViewModelProvider> _viewModelProvider = HashMap();
-
 extension ViewModelStoreOwnerExtension on LifecycleObserverRegistry {
   ViewModelStore getViewModelStore() =>
-      _viewModelStore.putIfAbsent(lifecycle, () {
+      lifecycleExtData.putIfAbsent(TypedKey<ViewModelStore>(), () {
         final store = ViewModelStore();
-        lifecycle.addObserver(LifecycleObserver.onEventDestroy(
-            (owner) => _viewModelStore.remove(owner.lifecycle)?.clear()));
+        makeLiveCancellable().onCancel.then((_) => store.clear());
         return store;
       });
 
   ViewModelProvider getViewModelProvider() {
     assert(currentLifecycleState > LifecycleState.destroyed,
         'Must be used before destroyed.');
-    return _viewModelProvider.putIfAbsent(lifecycle, () {
-      final provider = ViewModelProvider(getViewModelStore());
-      lifecycle.addObserver(LifecycleObserver.onEventDestroy(
-          (owner) => _viewModelProvider.remove(owner.lifecycle)));
-      return provider;
-    });
+    return lifecycleExtData.putIfAbsent(TypedKey<ViewModelProvider>(),
+        () => ViewModelProvider(getViewModelStore()));
   }
 
   T viewModels<T extends ViewModel>({ViewModelFactory<T>? factory}) {
@@ -184,118 +186,5 @@ extension ViewModelStoreOwnerExtension on LifecycleObserverRegistry {
       life = life.parent;
     }
     return null;
-  }
-}
-
-class LifecycleScope {
-  final LifecycleObserverRegistry _registry;
-
-  LifecycleScope(this._registry);
-
-  Future<R> launchOnState<R>(
-          {LifecycleState state = LifecycleState.started,
-          required FutureOr<R> Function() block}) =>
-      _registry.whenMoreThanState(state).then((value) => block());
-
-  Future<R> launchOnNextState<R>(
-          {LifecycleState state = LifecycleState.started,
-          required FutureOr<R> Function() block}) =>
-      _registry.nextLifecycleState(state).then((value) => block());
-
-  Future<R> launchOnStarted<R>({required FutureOr<R> Function() block}) =>
-      launchOnState(state: LifecycleState.started, block: block);
-
-  Future<R> launchOnResumed<R>({required FutureOr<R> Function() block}) =>
-      launchOnState(state: LifecycleState.resumed, block: block);
-
-  Future<R> launchOnNextStarted<R>({required FutureOr<R> Function() block}) =>
-      launchOnNextState(state: LifecycleState.started, block: block);
-
-  Future<R> launchOnNextResumed<R>({required FutureOr<R> Function() block}) =>
-      launchOnNextState(state: LifecycleState.resumed, block: block);
-}
-
-class _VMLifecycleObserver<VM extends ViewModel>
-    with LifecycleStateChangeObserver {
-  final VM _viewModel;
-
-  LaunchWhen<VM>? launchWhenCreated;
-  LaunchWhen<VM>? launchWhenStarted;
-  LaunchWhen<VM>? launchWhenResumed;
-  Map<LifecycleState, LaunchWhen<VM>> _repeatOn = {};
-
-  _VMLifecycleObserver(this._viewModel);
-
-  bool _firstCreate = true, _firstStart = true, _firstResume = true;
-
-  @override
-  void onStateChange(LifecycleOwner owner, LifecycleState state) {
-    if (_firstCreate &&
-        state == LifecycleState.created &&
-        launchWhenCreated != null) {
-      _firstCreate = false;
-      launchWhenCreated!(_viewModel);
-    } else if (_firstStart &&
-        state == LifecycleState.started &&
-        launchWhenStarted != null) {
-      _firstStart = false;
-      launchWhenStarted!(_viewModel);
-    } else if (_firstResume &&
-        state == LifecycleState.resumed &&
-        launchWhenResumed != null) {
-      _firstResume = false;
-      launchWhenResumed!(_viewModel);
-    }
-    _repeatOn[state]?.call(_viewModel);
-  }
-}
-
-Map<ViewModel, _VMLifecycleObserver> _vmObservers = weak.WeakMap();
-
-typedef LaunchWhen<VM extends ViewModel> = FutureOr Function(VM viewModel);
-
-extension LifecycleObserverRegistryWithLifecyle on LifecycleObserverRegistry {
-  void withLifecycleScopeViewModelLaunch<VM extends ViewModel>({
-    VM? viewModel,
-    ViewModelFactory<VM>? factory,
-    LaunchWhen<VM>? launchWhenCreated,
-    LaunchWhen<VM>? launchWhenStarted,
-    LaunchWhen<VM>? launchWhenResumed,
-    LaunchWhen<VM>? repeatOnStarted,
-    LaunchWhen<VM>? repeatOnResumed,
-  }) {
-    if (launchWhenCreated == null &&
-        launchWhenStarted == null &&
-        launchWhenResumed == null &&
-        repeatOnStarted == null &&
-        repeatOnResumed == null) {
-      return;
-    }
-
-    final vm = viewModel ?? viewModels<VM>(factory: factory);
-
-    final observer = _vmObservers.putIfAbsent(vm, () {
-      final o = _VMLifecycleObserver<VM>(vm);
-      o.launchWhenCreated = launchWhenCreated;
-      o.launchWhenStarted = launchWhenStarted;
-      o.launchWhenResumed = launchWhenResumed;
-
-      if (repeatOnStarted != null) {
-        o._repeatOn[LifecycleState.started] = repeatOnStarted;
-      }
-      if (repeatOnResumed != null) {
-        o._repeatOn[LifecycleState.resumed] = repeatOnResumed;
-      }
-
-      addLifecycleObserver(o);
-      return o;
-    }) as _VMLifecycleObserver<VM>;
-
-    if (repeatOnStarted != null) {
-      observer._repeatOn[LifecycleState.started] = repeatOnStarted;
-    }
-    if (repeatOnResumed != null) {
-      observer._repeatOn[LifecycleState.resumed] = repeatOnResumed;
-    }
   }
 }
